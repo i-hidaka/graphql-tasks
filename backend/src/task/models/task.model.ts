8966a36c300403ceac95acbebd0f3dfdc5e8e0a8
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-07-21 10:48:35 +0900

    コメント追加

--- backend/src/task/models/task.model.ts
+++ backend/src/task/models/task.model.ts
@@ -1,7 +1,9 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
+// クラスからGraphqlスキーマを作成する
 @ObjectType()
 export class Task {
+  // Graphqlスキーマ作成の際に、number型はデフォルトだとFloat型になってしまうので指定
   @Field(() => Int)
   id: number;
 
@@ -14,6 +16,7 @@ export class Task {
   @Field()
   status: 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED';
 
+  // nullを許容
   @Field({ nullable: true })
   description: string;
 }
