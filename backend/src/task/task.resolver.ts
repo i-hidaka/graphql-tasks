d5ee0ddf85717d4cf4ab20343d957fbf7d2b78d0
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-07-24 18:22:37 +0900

    タスク更新機能追加

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,10 +1,12 @@
 import { CreateTaskInput } from './dto/createTask.input';
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task as TaskModel } from './models/task.model';
 import { Task } from '@prisma/client';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 // controllerの役割
+// query データ取得（GET）、mutation データ追加、作成、削除（GET、POST、DELETE）
 @Resolver()
 export class TaskResolver {
   constructor(private readonly taskService: TaskService) {}
@@ -21,5 +23,12 @@ export class TaskResolver {
     @Args('createTaskInput') createTaskInput: CreateTaskInput,
   ): Promise<Task> {
     return await this.taskService.createTask(createTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async updateTask(
+    @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.updateTask(updateTaskInput);
   }
 }
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -2,6 +2,7 @@ import { Task } from '@prisma/client';
 import { PrismaService } from './../prisma/prisma.service';
 import { CreateTaskInput } from './dto/createTask.input';
 import { Injectable } from '@nestjs/common';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Injectable()
 export class TaskService {
@@ -18,6 +19,19 @@ export class TaskService {
         dueDate,
         description,
       },
+    });
+  }
+
+  async updateTask(updateTaskInput: UpdateTaskInput): Promise<Task> {
+    const { id, name, dueDate, status, description } = updateTaskInput;
+    return await this.prismaService.task.update({
+      data: {
+        name,
+        dueDate,
+        status,
+        description,
+      },
+      where: { id },
     });
   }
 }
